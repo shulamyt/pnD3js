f526eec6f59ae87e27e87b92e39ad0111de12509
Shulamyt Ajami <shulamyt.ajami@amdocs.com>
2018-07-03 10:51:59 +0300

    add Graph component

---
+++ src/components/graph.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Graph extends Component {
+  render() {
+    return(
+      <div>Graph</div>
+    );
+  }
+}
+
+export default Graph;
--- src/pnd.jsx
+++ src/pnd.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Graph from './components/graph';
 
 class Pnd extends Component {
   render() {
     return(
+    <div>
       <div>Hi pnd!</div>
+      <Graph/>
+    </div>
     );
   }
 }
